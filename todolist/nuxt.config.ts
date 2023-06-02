5eb01fe3e42d090b6e7fdc3916e04668a766c1a8
kimKyeongbeom <bibix321@naver.com>
2023-06-02 10:07:13 +0900

    SEO 개선

--- todolist/nuxt.config.ts
+++ todolist/nuxt.config.ts
@@ -11,4 +11,18 @@ export default defineNuxtConfig({
     dbPw: process.env.DATABASE_PASSWORD || "이건안알려줌",
     dbDb: process.env.DATABASE_DATABASE || "todolist",
   },
+  app: {
+    title: "todolist",
+    head: {
+      meta: [
+        {
+          name: "summary",
+          content: "nuxt3로 만든 todolist",
+        },
+        {
+          charset: "utf-8",
+        },
+      ],
+    },
+  },
 });
